6c85d518245bfdc055be09e431d7b50d507a5caf
Tobías Chavarría <tobias.chavarria.castro@gmail.com>
2024-12-01 20:52:30 +0100

    feat(pydantic-demo): add Fields validation

--- tutorials/pydantic_demo/main.py
+++ tutorials/pydantic_demo/main.py
@@ -38,7 +38,54 @@ def models_examples():
     print(f"Is the dict dump method ok: {employee_dump_dict == employee_as_dict}", "\n")
 
 
+def fields_example():
+    # EXAMPLE 1: Incorrect data
+    print("--- EXAMPLE 1: Incorrect data ---")
+    incorrect_employee_data = {
+        "name": "",
+        "email": "tobias@pydantik.com",
+        "birth_date": "1995-12-02",
+        "salary": -10,
+        "department": "IT",
+        "elected_benefits": True,
+    }
+
+    try:
+        Employee.model_validate(incorrect_employee_data)
+    except Exception as e:
+        print(f"Error: {e}\n")
+
+    # EXAMPLE 2: Correct data
+    print("\n--- EXAMPLE 2: Correct Data ---")
+    employee_data = {
+        "name": "Tobias",
+        "email": "tobias@pydantic.com",
+        "birth_date": "1995-12-02",
+        "salary": 1000,
+        "department": "ENGINEERING",
+        "elected_benefits": True,
+    }
+
+    test_employee = Employee.model_validate(employee_data)
+    print(f"Created Employee: {test_employee}\n")
+
+    # EXAMPLE 2.1: Getting `repr=False` values
+    print("--- EXAMPLE 2.1: Getting `repr=False` values ---")
+    print(f"Getting DoB value: {test_employee.date_of_birth}")
+    print(f"Getting salary value: {test_employee.salary}\n")
+
+    # EXAMPLE 2.2: Updating `frozen=True` values
+    print("--- EXAMPLE 2.2: Updating `frozen=True` values ---")
+    try:
+        test_employee.name = "Jesus"
+    except Exception as e:
+        print(f"Error: {e}\n")
+
+
 if __name__ == "__main__":
 
     # Creating models from: explict values, dict
     models_examples()
+
+    # Using fields
+    fields_example()
--- tutorials/pydantic_demo/models.py
+++ tutorials/pydantic_demo/models.py
@@ -2,7 +2,7 @@ from datetime import date
 from enum import Enum
 from uuid import UUID, uuid4
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
 class Department(Enum):
@@ -13,10 +13,11 @@ class Department(Enum):
 
 
 class Employee(BaseModel):
-    employee_id: UUID = uuid4()
-    name: str
-    email: EmailStr
-    date_of_birth: date
-    salary: float
+    model_config = ConfigDict(populate_by_name=True)
+    employee_id: UUID = Field(default_factory=uuid4, frozen=True)
+    name: str = Field(min_length=1, frozen=True)
+    email: EmailStr = Field(pattern=r".+@pydantic\.com$")
+    date_of_birth: date = Field(alias="birth_date", repr=False, frozen=True)
+    salary: float = Field(gt=0, repr=False)
     department: Department
     elected_benefits: bool
